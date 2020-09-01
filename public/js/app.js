a8ba2c9f64296df267a2a31a7d4a21da4564d30f
Damian Chanas <damianchanas@Damians-MacBook-Air.local>
2020-09-01 21:57:55 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,16 @@ weatherForm.addEventListener("submit", (e) => {
       const location = search.value;
       messageOne.textContent = "Loading data!";
       messageTwo.textContent = "";
-      fetch("http://localhost:3000/weather?address=" + location).then(
-            (response) => {
-                  response.json().then((data) => {
-                        if (data.error) {
-                              messageOne.textContent = data.error;
-                        } else {
-                              console.log(data.location);
-                              console.log(data.forecast);
-                              messageOne.textContent = data.location;
-                              messageTwo.textContent = data.forecast;
-                        }
-                  });
-            }
-      );
+      fetch("/weather?address=" + location).then((response) => {
+            response.json().then((data) => {
+                  if (data.error) {
+                        messageOne.textContent = data.error;
+                  } else {
+                        console.log(data.location);
+                        console.log(data.forecast);
+                        messageOne.textContent = data.location;
+                        messageTwo.textContent = data.forecast;
+                  }
+            });
+      });
 });
